11e792dbd86d5d662ffd1328a70116da0cebd225
Pearl Lee <pearl.lee.songee@gmail.com>
2022-04-11 13:41:57 +0900

    종목 분석 디테일 로딩 UI 추가

--- src/components/StockAnalysis/Detail.tsx
+++ src/components/StockAnalysis/Detail.tsx
@@ -1,7 +1,10 @@
 import { observer } from 'mobx-react';
+import { Skeleton, Box, CircularProgress } from '@mui/material';
+
 import DetailHeader from './DetailHeader';
 import DetailCharts from './DetailCharts';
 import { useStockAnalysisStore } from '../../providers/StockStoreProvider';
+import Style from '../../styles/detail.module.scss';
 
 export default observer(function Detail() {
     const stockAnalysisStore = useStockAnalysisStore();
@@ -9,7 +12,15 @@ export default observer(function Detail() {
 
     return(<section className="detailContainer">
         {headerData === null && 
-            <p>데이터 없음</p>
+            <Box className={Style.loading}>
+                <div>
+                    <Skeleton width="30%" height="4em" animation="wave" />
+                    <Skeleton width="50%" height="2em" animation="wave" />
+                </div>
+                <div>
+                    <CircularProgress />
+                </div>
+            </Box>
         }
         {headerData !== null &&
             <>
