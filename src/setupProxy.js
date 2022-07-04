69c82bbd984b5a2492daccc9af3313340758155a
이진주 <33019918+PearlLee@users.noreply.github.com>
2022-07-04 08:01:40 +0000

    proxy 설정 환경변수에서 인식하도록 수정

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,10 +1,11 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
+const PROXY_HOST = process.env.REACT_APP_PROXY_HOST || 'http://localhost:8000';
 module.exports = function(app) {
     app.use(
         '/api',
         createProxyMiddleware({
-            target: 'http://localhost:8000',
+            target: PROXY_HOST,
             changeOrigin: true,
         })
     );
